946bfd84d2d592fa4426f229826ad32024627eff
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-04-21 11:57:40 +0530

    const page should be there

--- include/page/page.hpp
+++ include/page/page.hpp
@@ -6,4 +6,5 @@ namespace statdb {
 constexpr static std::size_t page_size = 1024 * 4; // 4 KB
 
 using page_t = std::span<std::byte, page_size>;
+using const_page_t = std::span<std::byte const, page_size>;
 } // namespace statdb
