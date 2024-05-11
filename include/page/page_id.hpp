f697ed8d54f9d188d33e369ba4e740fba997040e
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-05-11 13:02:18 +0530

    page id to have constructor

--- include/page/page_id.hpp
+++ include/page/page_id.hpp
@@ -6,6 +6,9 @@ struct page_id {
   std::string file_path;
   std::size_t offset;
 
+  page_id(std::string file_path_ = {}, std::size_t offset_ = {})
+      : file_path(std::move(file_path_)), offset(offset_) {}
+
   friend bool operator==(page_id const &, page_id const &) = default;
 };
 } // namespace statdb
