e84ad1d458ab940b0cc44b362cd58b63e49f1300
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-05-11 12:31:36 +0530

    strong typing to page

--- include/page/page.hpp
+++ include/page/page.hpp
@@ -6,5 +6,5 @@
 namespace statdb {
 constexpr static std::size_t page_size = 1024 * 4; // 4 KB
 
-using page = std::array<std::byte, page_size>;
+struct page : public std::array<std::byte, page_size> {};
 } // namespace statdb
