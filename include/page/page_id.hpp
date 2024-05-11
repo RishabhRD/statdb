aed99aa5eb9e48cb542330e7f6a11d625873690e
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-05-11 12:59:53 +0530

    giving a basic definition to page_id

--- include/page/page_id.hpp
+++ include/page/page_id.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <string>
 namespace statdb {
 struct page_id {
+  std::string file_path;
+  std::size_t offset;
+
   friend bool operator==(page_id const &, page_id const &) = default;
 };
 } // namespace statdb
